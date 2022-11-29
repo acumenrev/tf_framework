f6f555e449fc6477c389347285cc8b3549b32bc7
Tri <acumen.us@gmail.com>
2022-11-29 07:31:17 +0700

    Update TFFramework

--- tf_framework/lib/models/base_error.dart
+++ tf_framework/lib/models/base_error.dart
@@ -1,7 +1,9 @@
 
+import 'package:dio/dio.dart';
+
 abstract class ITFError {
-  late int statusCode;
-  late String message;
+  int? statusCode;
+  String? message;
   late dynamic? data;
   late String domainName;
 }
@@ -11,15 +13,23 @@ class TFError implements ITFError {
   late var data;
 
   @override
-  late String message;
+  String? message;
 
   @override
-  late int statusCode;
+  int? statusCode;
 
   @override
   late String domainName;
 
   TFError({required this.statusCode, required this.message, required this.domainName , this.data});
+
+  TFError.initFromDioError(DioError err) {
+    if (err.response != null) {
+      this.statusCode = err.response!.statusCode;
+      this.message = err.response!.statusMessage;
+      this.data = err.response!.data;
+    }
+  }
 
   String toString() {
     return "status code: $statusCode, domain: $domainName, message: $message";
---
+++ tf_framework/lib/models/tf_network_response_model.dart
@@ -0,0 +1,24 @@
+
+import 'package:dio/dio.dart';
+import 'package:tf_framework/models/base_error.dart';
+
+class TFNetworkResponseModel {
+  Response? _response;
+  TFError? _error;
+
+  setResponse(Response res) {
+    _response = res;
+  }
+
+  setError(TFError error) {
+    _error = _error;
+  }
+
+  getResponse() {
+    return _response;
+  }
+
+  getError() {
+    return _error;
+  }
+}
