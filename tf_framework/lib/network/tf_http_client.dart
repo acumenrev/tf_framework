d5ba7ba5a9a2dcf96b5692b251d4b0b53da5d4bd
Tri Vo <tri.vo@adurolife.com>
2022-12-08 11:20:58 +0700

    Update ver 0.0.2 - Dependency Injection for TFHTTPClient

--- tf_framework/lib/network/tf_http_client.dart
+++ tf_framework/lib/network/tf_http_client.dart
@@ -1,22 +1,13 @@
-
 import 'package:dio/dio.dart';
 import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:tf_framework/models/base_error.dart';
 import 'package:tf_framework/models/tf_network_response_model.dart';
 import 'package:tf_framework/utils/tf_logger.dart';
-enum TFHTTPMethod {
-  get,
-  post,
-  patch,
-  put,
-  delete
-}
 
+enum TFHTTPMethod { get, post, patch, put, delete }
 
-class TFS extends TFHTTPClient {
-
-}
+class TFS extends TFHTTPClient {}
 
 class TFHTTPClient {
   late Dio _client;
@@ -30,9 +21,13 @@ class TFHTTPClient {
     return 3000; // 3s
   }
 
+  TFHTTPClient({Dio? dio}) {
+    if (dio == null) {
+      _client = Dio(_dioOptions());
+    } else {
+      _client = dio;
+    }
 
-  TFHTTPClient() {
-    _client = Dio(_dioOptions());
     _setupInterceptors();
   }
 
@@ -64,25 +59,32 @@ class TFHTTPClient {
   }
 
   setupCacheForBaseUrl(String url) {
-    _client.interceptors.add(DioCacheManager(CacheConfig(baseUrl: url)).interceptor);
+    _client.interceptors
+        .add(DioCacheManager(CacheConfig(baseUrl: url)).interceptor);
   }
 
   _dioOptions() {
     var dioOptions = BaseOptions(
-      connectTimeout: getConnectTimeout(),
-      receiveTimeout: getReceiveTimeout()
-    );
+        connectTimeout: getConnectTimeout(),
+        receiveTimeout: getReceiveTimeout());
     return dioOptions;
   }
-
 
   /// Make a network request
   ///
   /// [path] request route
   ///
   /// [headers] Custom headers for the request
-  Future<TFNetworkResponseModel> fetch({required String path, required TFHTTPMethod method ,Map<String, dynamic>? headers, dynamic data}) async {
-    var requestOptions = RequestOptions(path: path, method: _convertHttpMethodEnumToString(method), headers: headers, data: data);
+  Future<TFNetworkResponseModel> fetch(
+      {required String path,
+      required TFHTTPMethod method,
+      Map<String, dynamic>? headers,
+      dynamic data}) async {
+    var requestOptions = RequestOptions(
+        path: path,
+        method: _convertHttpMethodEnumToString(method),
+        headers: headers,
+        data: data);
     TFNetworkResponseModel networkResponse = TFNetworkResponseModel();
     try {
       Response res = await _client.fetch(requestOptions);
